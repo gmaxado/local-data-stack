71137ad4f6b49176c70e1284a04edf0b1554f688
gabrielmbarbosa <gabriel.barbosa@autoglass.com.br>
2026-07-05 12:54:38 -0300

    M1 pronto - script de ingestao IEEE-FRAUD com particionamento na camada bronze

--- src/local_data_stack/ingestion/ieee_fraud.py
+++ src/local_data_stack/ingestion/ieee_fraud.py
@@ -1,6 +1,36 @@
-from pathlib import Path
 import polars as pl
 from local_data_stack.config import get_data_layer_paths
-from local_data_stack.io_utils import read_csv_to_parquet
+from local_data_stack.io_utils import read_csv, write_parquet
 
-DIC_PATH = get_data_layer_paths()
+def ingest_ieee_fraud() -> None:
+
+    # Locate paths:
+    dic_path = get_data_layer_paths()
+    raw_dir = dic_path['raw']
+    bronze_dir = dic_path['bronze']
+    transaction_csv_path = raw_dir / 'ieee-fraud-detection' / 'train_transaction.csv'
+    identity_csv_path = raw_dir / 'ieee-fraud-detection' / 'train_identity.csv'
+
+    # Transaction Data
+    transaction_lazy_frame = read_csv(transaction_csv_path)
+    transaction_lazy_frame_partitioned = (
+        transaction_lazy_frame
+        .with_columns(
+            (pl.col('TransactionDT')/86400).cast(pl.Int64).alias('TransactionDT_RELATIVE_DAY')
+        )
+    )
+    write_parquet(
+        lazy_frame=transaction_lazy_frame_partitioned,
+        path_output=bronze_dir / 'transaction',
+        partition_by_ordered_cols=['TransactionDT_RELATIVE_DAY']
+    )
+
+    # Identity Data
+    identity_lazy_frame = read_csv(identity_csv_path)
+    write_parquet(
+        lazy_frame=identity_lazy_frame,
+        path_output=bronze_dir / 'identity',
+    )
+
+if __name__ == '__main__':
+    ingest_ieee_fraud()
